16b81c71c12f6125f1f693ce966a7b3011528d1c
strugglinglee <strugglinglee@163.com>
2022-03-24 09:38:11 +0800

    learn: 代码分离

--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
-import _ from 'lodash';
-import printMe from './print';
+// import _ from 'lodash';
+// import printMe from './print';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
-function component() {
+async function getComponent() {
   let element = document.createElement('div');
-  let btn = document.createElement('button');
-
+  const { default: _ } = await import(/*webpackChunkName: "lodash" */ 'lodash');
   element.innerHTML = _.join(['Hello1', 'Webpack'], '');
-
-  btn.innerHTML = '点击这里，查看console';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
 
   return element;
 }
 
-// document.body.appendChild(component());
-let element = component();
-document.body.appendChild(element);
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
 
 if (module.hot) {
   module.hot.accept('./print.js', function () {
     console.log('Accepting the updated printMe module!');
     // printMe();
-    document.body.removeChild(element);
-    element = component();
-    element = component();
-    document.body.appendChild(element);
+    // document.body.removeChild(element);
+    // element = component();
+    // element = component();
+    // document.body.appendChild(element);
   });
 }
--- webpack.common.js
+++ webpack.common.js
@@ -1,35 +1,36 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
+    app: './src/index.js',
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Production",
-    })
+      title: 'Production',
+    }),
   ],
 };
